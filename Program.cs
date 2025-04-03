67df4971f887a3c708d6e21ce19cc32ee3451b5d
maximra <70347652+maximra@users.noreply.github.com>
2025-04-04 01:06:24 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿
+using FileOrganizerSoftware;
+using Serilog;
+using System;
+using System.Net.Http.Headers;
+
+public class FileOrganizerApp
+{
+    public static void Main()
+    {
+        try
+        {
+            if(!Directory.Exists("logs"))
+                Directory.CreateDirectory("logs");
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine($"Error: {e.Message}");
+            return ;                                       
+        }
+        AppLogger.Instance.Information("Logging information started.");
+
+
+
+
+        FileOrganizerSoftware.MenuController softwareTester = new FileOrganizerSoftware.MenuController("unvalid$$$", "unvalid$$$");  
+        softwareTester.StartSequence();
+        softwareTester.ModeSelection();
+        softwareTester.InsertSourceDirectory();
+        softwareTester.InsertDestinationDirectory();
+        softwareTester.OperationMenuSelection();
+
+        AppLogger.Instance.Information("Logging infromation ended.");
+
+        AppLogger.Instance.CloseAndFlush();
+
+
+    }
+}
